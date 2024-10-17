98ae68bd4beec646c76b86b8f02688567889db8a
Dov Kruger <Dov.Kruger@gmail.com>
2024-10-17 08:59:38 -0400

    fixed the string bug

--- hw/ALU_8bit_tb.sv
+++ hw/ALU_8bit_tb.sv
@@ -4,16 +4,24 @@ module ALU_8bit_tb();
   reg [7:0] b;
   reg [2:0] op;
   wire [7:0] out;
-  string op_str[8] = {"add", "sub", "and", "or", "xor", "not", "shl", "shr"};
+  string op_str[8];
   ALU_8bit uut(.a(a), .b(b), .op(op), .out(out));
   initial begin
     integer i;
+    op_str[0] = "add";
+    op_str[1] = "sub";
+    op_str[2] = "and";
+    op_str[3] = "or ";
+    op_str[4] = "xor";
+    op_str[5] = "not";
+    op_str[6] = "shl";
+    op_str[7] = "shr";
     a = 13;
     b = 37;
     op = 0;
     for (i = 0; i < 8; i++) begin
       #3
-      $display("op=%b out=%b", op, out);
+      $display("%s op=%b out=%b", op_str[op], op, out);
       op++;
     end
   #3
@@ -22,7 +30,7 @@ module ALU_8bit_tb();
     op = 0;
     for (i = 0; i < 8; i++) begin
       #3
-      $display("op=%b out=%b", op, out);
+      $display("%s op=%b out=%b", op_str[op], op, out);
       op++;
     end
     $finish;
