7a8b0a9e50184fe1044a40909370dbbd335babfc
Dov Kruger <dkruger@gmail.com>
2024-12-09 15:30:45 -0500

    moved in new homework spec

--- verilog/02intermediate/ALU64.sv
+++ verilog/02intermediate/ALU64.sv
@@ -2,8 +2,8 @@ module ALU64(
     input wire[63:0] a,
     input wire[63:0] b,
     input wire[3:0] op,
-    output reg[63:0] result
-    input wire clk,
+    output reg[63:0] result,
+    input wire clk
     );
 
     always @(posedge clk) begin
@@ -26,4 +26,5 @@ module ALU64(
         4'hF:result = 0;
         endcase
 
-    end 
+    end
+endmodule
