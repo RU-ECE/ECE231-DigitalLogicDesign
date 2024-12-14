24723a6a3442814a96bfff88f42eb8ff41ba5c03
PoeticJohnny4 <102179998+PoeticJohnny4@users.noreply.github.com>
2024-12-13 23:27:00 -0500

    Update ALU64.sv

--- hw/ALU64.sv
+++ hw/ALU64.sv
@@ -1,34 +1,32 @@
 /*
- Author: _______________________
- cite: ________________________
- Your name and citation of sources goes above!
+ Author: _________JOHN ALLEN IV______________
+ cite: ___________CHATGPT/Google AI _____________
  */
-module ALU64(
-    input wire[63:0] a,
+nput wire[63:0] a,
     input wire[63:0] b,
     input wire[3:0] op,
-    output reg[63:0] result
-    input wire clk,
+    output reg[63:0] result,
+    input wire clk
     );
 
-    always @(posedge clk) begin
-        case(op)
-        4'h0: result = a + b;
-        4'h1: result = 0;
-        4'h2:result = 0;
-        4'h3:result = 0;
-        4'h4:result = 0;
-        4'h5:result = 0;
-        4'h6:result = 0;
-        4'h7:result = 0;
-        4'h8:result = 0;
-        4'h9:result = 0;
-        4'hA:result = 0;
-        4'hB:result = 0;
-        4'hC:result = 0;
-        4'hD:result = 0;
-        4'hE:result = 0;
-        4'hF:result = 0;
-        endcase
-
-    end 
+always @(posedge. clk begin
+ case (op)
+    4'h0: result = a + b;
+    4'h1: result = a - b;
+    4'h2: result = a * b;
+    4'h3: result = a & b;
+    4'h4: result:= a | b;
+    4'h5: result = a ^ b;
+    4'h6: result = !a;
+    4'h7: result = a < b [5:0];
+    4'h8: result = a >> b［5:01；
+    4'h9: result = a * a;
+    4 hA: result = (a » b[5:01) | (a < (64 - b[5:0}));
+    4'hB: result = (a < b[5:0]) |(a>>(64 - b[5:01 ));
+    4'hC: result = 64'b0;
+    4'hD: result = 64'b0;
+    4"hE: result: = 64'b0;
+    4'hF: result = 64'b0;
+endcase
+end
+endmodule
